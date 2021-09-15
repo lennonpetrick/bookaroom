297a1aa02787dec9351eda3bf7513b5cb74d2564
Lennon Spirlandelli <lennonpetrick@gmail.com>
2021-09-15 19:03:09 +0200

    Added method to map model to entity

--- app/src/main/java/com/example/bookaroom/data/mapper/RoomMapper.kt
+++ app/src/main/java/com/example/bookaroom/data/mapper/RoomMapper.kt
@@ -8,4 +8,8 @@ internal class RoomMapper @Inject constructor() {
     fun convert(entity: RoomEntity): Room {
         return Room(entity.name, entity.spots, entity.thumbnail)
     }
+
+    fun convert(model: Room): RoomEntity {
+        return RoomEntity(model.name, model.spots, model.thumbnail)
+    }
 }
--- app/src/test/java/com/example/bookaroom/data/mapper/RoomMapperTest.kt
+++ app/src/test/java/com/example/bookaroom/data/mapper/RoomMapperTest.kt
@@ -1,6 +1,7 @@
 package com.example.bookaroom.data.mapper
 
 import com.example.bookaroom.data.entities.RoomEntity
+import com.example.bookaroom.domain.Room
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
@@ -23,7 +24,25 @@ internal class RoomMapperTest {
         })
     }
 
+    @Test
+    fun `When converting a room model to an entity, then it converts properly`() {
+        val model = createRoomModel()
+        val entity = subject.convert(model)
+
+        assertAll("Room fields", {
+            with(entity) {
+                assertEquals(model.name, name)
+                assertEquals(model.spots, spots)
+                assertEquals(model.thumbnail, thumbnail)
+            }
+        })
+    }
+
     private fun createRoomEntity(): RoomEntity {
         return RoomEntity(name = "name", spots = 1, thumbnail = "imageUrl")
+    }
+
+    private fun createRoomModel(): Room {
+        return Room(name = "name", spots = 1, thumbnail = "imageUrl")
     }
 }
